1f1b345be6f5e35b2583c0ab29f9d2b16eaa46a4
RetroMelon <JoeFrew95@gmail.com>
2014-10-11 02:28:07 +0100

    made correct url redirects for the api and main app. also created placeholder views for the query and context api calls.

---
+++ djangobackend/api/urls.py~
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+#To make a call to the api, we should do /api/<'context' or 'query'>/?q=some+query
+urlpatterns = patterns('api.views',
+    (r'^test/', 'api_test'),
+    (r'^query/$', 'api_query'),
+    (r'^context/', 'api_context'),
+    (r'^$', 'api_call'),
+)
--- djangobackend/djangobackend/urls.py
+++ djangobackend/djangobackend/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+#We redirect all api requests on to the api app, and all requests to the main page to the main app.
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'djangobackend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    (r'^api/', include('api.urls')),
     (r'^$', include('main.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
