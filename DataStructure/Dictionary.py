a9ef321aa12b1a276960c94b9f012ee2098f5251
Alexandru Dochioiu <2061919d@student.gla.ac.uk>
2014-10-11 18:07:55 +0100

    Update Dictionary.py

--- DataStructure/Dictionary.py
+++ DataStructure/Dictionary.py
@@ -1,24 +1,23 @@
 import csv
 import string
-#import pickle
 
 data = {}
 meaning = {}
-meaning['country'] = {}
-meaning['region'] = {}
+meaning['country'] = {"" : ""}
+meaning['region'] = {"" : ""}
 # meaning criterion
-meaning['alternative'] = {"1": "Insurgency/Guerilla Action", "2": "Other Crime Type", "3": "Intra/Inter-group Conflict", "4": "Lack of Intentionality"}
-meaning['atack_type'] = {'1': 'Assassination', '2': 'Armed Assault', '3': 'Bombing/Explosion', '4': 'Hijacking', '5': 'Hostage Taking (Barricade Incident)',
+meaning['alternative'] = {"" : "", "1": "Insurgency/Guerilla Action", "2": "Other Crime Type", "3": "Intra/Inter-group Conflict", "4": "Lack of Intentionality", "5" : "State Actors"}
+meaning['atack_type'] = {"": "", '1': 'Assassination', '2': 'Armed Assault', '3': 'Bombing/Explosion', '4': 'Hijacking', '5': 'Hostage Taking (Barricade Incident)',
                          '6': 'Hostage Taking (Kidnapping)', '7':'Facility/Infrastructure Attack', '8': 'Unarmed Assault', '9': 'Unknown'}
-meaning['target_type'] = {}
-meaning['target_subtype'] = {}
-meaning['nationality'] = {}
-meaning['claim_mode'] = {}
-meaning['weapon_type'] = {}
-meaning['weapon_subtype'] = {}
-meaning['property'] = { '1': 'Catastrophic (likely > $1 billion)', '2': 'Major (likely > $1 million but < $1 billion)',
+meaning['target_type'] = {"" : ""}
+meaning['target_subtype'] = {"" : ""}
+meaning['nationality'] = {"" : ""}
+meaning['claim_mode'] = {"" : ""}
+meaning['weapon_type'] = {"" : ""}
+meaning['weapon_subtype'] = {"" : ""}
+meaning['property'] = { "" : "", '1': 'Catastrophic (likely > $1 billion)', '2': 'Major (likely > $1 million but < $1 billion)',
                         '3': 'Minor (likely < $1 million)', '4': 'Unknown'}
-meaning['hostages_outcome'] = {}
+meaning['hostages_outcome'] = {"" : ""}
 
 
 fields = []
@@ -78,12 +77,8 @@ def reader():
                 data[key]['success'] = row[26]
                 data[key]['suicide'] = row[27]
 
-                data[key]['atacktype'] = [row[28], row[30], row[32]]
+                data[key]['atacktype'] = [row[29], row[31], row[33]]
 
-                data[key]['target'] = [{'type': [row[34], row[42], row[50]] , 'subtype': [row[36], row[44], row[52]],
-                                        'corporation': [row[38], row[46], row[54]], 'target': [row[39], row[47], row[55]],
-                                        'nationality': [row[40], row[48], row[56]]}, #nationality from meaning['country'][...]
-                                       ]
                 if row[34] not in meaning['target_type'].keys() and row[34] != '':
                     meaning['target_type'][row[34]] = row[35]
                 if row[36] not in meaning['target_type'].keys() and row[36] != '':
@@ -102,6 +97,10 @@ def reader():
                     meaning['target_subtype'][row[52]] = row[53]
                 if row[56] not in meaning['nationality'].keys() and row[56] != '':
                     meaning['nationality'][row[56]] = row[57]
+                data[key]['target'] = [{'type': [row[35], row[43], row[51]] , 'subtype': [row[37], row[45], row[53]],
+                                        'corporation': [row[38], row[46], row[54]], 'target': [row[39], row[47], row[55]],
+                                        'nationality': [row[41], row[49], row[57]]}, #nationality from meaning['country'][...]
+                                       ]
 
                 data[key]['group_name'] = [row[58], row[60], row[62]]
                 data[key]['group_subname'] = [row[59], row[61], row[63]]
@@ -109,20 +108,24 @@ def reader():
                 data[key]['motive'] = row[64]
 
                 data[key]['group_confirmed'] = [row[65], row[66], row[67]]
-
+                if (row[68]=="-99" or row[68]==""):
+                    row[68]="Unknown"
                 data[key]['perps'] = row[68]    #-99 is unknown; blank is unknown
+                if (row[69]=="-99" or row[69]==""):
+                    row[69]="Unknown"
                 data[key]['perpsCaptured'] = row[69]
 
-                data[key]['claimed'] = [[row[70], row[71]], [row[73], row[74]], [row[76], row[77]]] #0/1, mode(1-10)
+                
                 if row[71] not in meaning['claim_mode'] and row[71] != '':
                     meaning['claim_mode'][row[71]] = row[72]
                 if row[74] not in meaning['claim_mode'] and row[74] != '':
                     meaning['claim_mode'][row[74]] = row[75]
                 if row[77] not in meaning['claim_mode'] and row[77] != '':
                     meaning['claim_mode'][row[77]] = row[78]
-
-                data[key]['weapon']={'type': [row[80], row[84], row[88], row[92]],
-                                     'subtype': [row[82], row[86], row[90], row[94]],
+                data[key]['claimed'] = [[row[70], row[72]], [row[73], row[75]], [row[76], row[78]]] #0/1, mode(1-10)
+                
+                data[key]['weapon']={'type': [row[81], row[85], row[89], row[93]],
+                                     'subtype': [row[83], row[87], row[91], row[95]],
                                      'details': row[96]}
                 if row[80] not in meaning['weapon_type'].keys() and row[80] != '':
                     meaning['weapon_type'][row[80]] = row[81]
@@ -148,7 +151,7 @@ def reader():
                 data[key]['property'] = [row[103], row[104], row[106], row[107]] #row[103] is 1/0/-9 = yes/now/unknown
                     # yes/no/unk , null or 1-4, value, comment
 
-                data[key]['hostages'] = [row[108], row[109], row[110], row[111], row[112], row[121], row[123]]
+                data[key]['hostages'] = [row[108], row[109], row[110], row[111], row[112], row[122], row[123]]
                     #yes/no null , nr host, nr host US, hours, days , outcome key, released
                 if row[121] not in meaning['hostages_outcome'].keys() and row[121] != '':
                     meaning['hostages_outcome'][row[121]] = row[122]
@@ -160,25 +163,13 @@ def reader():
                 data[key]['sources'] = [row[125], row[126], row[127]]
                 data[key]['related'] = row[133].split(',')
                 
+                
             i = False
                 
 
-
         f.close()
+
+
 
 reader()
 
-#f=open("db.pck","w")
-#g=open("meaning","w")
-#pickle.dump(data,f)
-#pickle.dump(meaning, g)
-#f.close()
-#g.close()
-        #if i==10000:
-            #break
-
-
-    
-        
-        
-        
